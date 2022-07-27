74f432e703e2307a5637376275a3f0f69279d91b
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2022-07-27 19:43:09 +0200

    restructure repo

--- sources/install.bat
+++ sources/install.bat
@@ -1,3 +1,2 @@
-powershell.exe -executionpolicy bypass -command "& '.\IntuneSystemTrayV2.ps1'"
-
+powershell.exe -executionpolicy bypass -command "& '.\Install-IntuneSystemTrayV2.ps1'"
 schtasks /Create /XML StartCompanyPortalSystemtray.xml /TN StartCompanyPortalSystemtray
